2f26966a11f680744ad8a57002b5cf660046a760
Rasul <rasul.zeynalov.95@gmail.com>
2022-07-06 16:48:09 +0400

    edited

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,10 +3,14 @@ import "./about.scss"
 const About = () => {
   return (
     <div className="about">
-      <h1 className="header">Who I am</h1>
-      <p className="description">My name is Rasul. I'm web developer based in Baku, Azerbaijan.</p>
-      <p></p>
-
+      <div className="first">
+        <h1 className="header">Who I am</h1>
+        <p className="description">My name is Rasul. I'm web developer based in Baku, Azerbaijan.</p>
+        <p className="desc2">During the day I work as a lead developer at a local agency and in the evening I work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.</p>
+      </div>
+      <div className="education">
+        <h1>Education</h1>
+      </div>
     </div>
   )
 }
---
+++ src/data/edicationData.js
@@ -0,0 +1,16 @@
+export const educationData = [
+    {
+        id: 1,
+        institution: 'Baku State University',
+        course: 'Bachelor of Technology',
+        startYear: '2015',
+        endYear: '2019'
+    },
+    {
+        id: 3,
+        institution: 'Hawking University, Cambridge',
+        course: 'Master of Technology',
+        startYear: '2019',
+        endYear: 'Present'
+    },
+]
