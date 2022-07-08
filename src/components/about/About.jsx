7f6f1efc0c70d040b6dd0a2f4bf4dc653915eb93
Rasul <rasul.zeynalov.95@gmail.com>
2022-07-08 23:06:16 +0400

    edited

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,4 +1,7 @@
-import "./about.scss"
+import "./about.scss";
+import { educationData } from "../../data/educationData";
+import {faGraduationCap} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 const About = () => {
   return (
@@ -9,6 +12,16 @@ const About = () => {
         <p className="desc2">During the day I work as a lead developer at a local agency and in the evening I work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.</p>
       </div>
       <div className="education">
+        {educationData.map(edu => (
+          <div className="card">
+            <FontAwesomeIcon style={{color:"black",fontSize:"32px",marginLeft:"15px"}} icon={faGraduationCap} />
+            <div style={{display:"flex",flexDirection:"column",marginLeft:"20px"}}>
+            <h5 style={{display:"block",fontSize:"15px",marginBottom:"10px"}}>{edu.startYear}-{edu.endYear}</h5>
+            <h2 style={{marginBottom:"5px"}}>{edu.institution}</h2>
+            <h4>{edu.course}</h4>
+            </div>
+          </div>
+        ))}
         <h1>Education</h1>
       </div>
     </div>
---
+++ src/data/educationData.js
@@ -0,0 +1,16 @@
+export const educationData = [
+    {
+        id: 1,
+        institution: 'Baku State University',
+        course: 'Bachelor',
+        startYear: '2012',
+        endYear: '2016'
+    },
+    {
+        id: 2,
+        institution: 'Azerbaijan Technical University',
+        course: 'Master degree',
+        startYear: '2016',
+        endYear: '2018'
+    },
+]
