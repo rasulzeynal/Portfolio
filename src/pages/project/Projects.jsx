9e66caf855faa127fbc5da4c260bd97a49f94a24
Rasul <rasul.zeynalov.95@gmail.com>
2022-07-23 00:03:34 +0400

    edted

--- src/pages/project/Projects.jsx
+++ src/pages/project/Projects.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import Particle2 from '../Particle2'
+import Particle2 from '../Particle2';
+import { Container, Row, Col, Card} from 'reactstrap';
+import {projectData} from "../../data/pojectData";
 
 const Projects = () => {
   return (
-    <div>
-       <Particle2/> 
-    </div>
+    <Container>
+       <Particle2/>
+       <Container>
+        <h1 className='project-heading'>
+          My Recent <strong className='purple'>Works</strong>
+        </h1>
+        <p style={{color: "white"}}>
+        Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            {projectData.map((project) => {
+              <Card>test</Card>
+            })}
+          </Col>
+        </Row>
+        </Container> 
+    </Container>
     
   )
 }
