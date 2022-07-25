d931b33fbe14ca4aef39ac7205b2c223dccfbc36
Rasul <rasul.zeynalov.95@gmail.com>
2022-07-25 16:50:34 +0400

    some components changed

--- src/pages/project/Projects.jsx
+++ src/pages/project/Projects.jsx
@@ -6,17 +6,16 @@ import {projectData} from "../../data/pojectData";
 
 const Projects = () => {
   return (
-    <Container fluid className="project-section">
+    <Container fluid className="project-section" style={{backgroundColor:"#150b27",position:"absolute"}}>
        <Particle2/>
-       <Container>
-        <h1 className='project-heading' style={{textAlign:"center"}}>
+       <Container >
+        <h1 className='project-heading' style={{textAlign:"center",marginTop:"100px"}}>
           My Recent <strong className='purple'>Works</strong>
         </h1>
-        <p style={{color: "white",textAlign:"center"}}>
+        <p style={{textAlign:"center",color:"white"}}>
         Here are a few projects I've worked on recently.
         </p>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card" style={{display:"flex",flexWrap:"wrap"}}>
+        <Row style={{paddingBottom: "10px" }} className="rows">
             {projectData.map((project) => (
               <ProjectCard 
               imgPath = {project}
@@ -26,7 +25,6 @@ const Projects = () => {
               demoLink="https://chatify-49.web.app/"
               />
             ))}
-          </Col>
         </Row>
         </Container> 
     </Container>
