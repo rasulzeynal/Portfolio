cae48b2045d8a8bb320cab409b798265fe3d2ba2
Rasul <rasul.zeynalov.95@gmail.com>
2022-07-29 12:41:13 +0400

    added github calendar

--- src/pages/about/Github.jsx
+++ src/pages/about/Github.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import GitHubCalendar from "react-github-calendar";
+import { Row } from "reactstrap";
 
 const Github = () => {
   return (
-    <div>Github</div>
+    <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+      <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
+        Days I <strong className="purple">Code</strong>
+      </h1>
+      <GitHubCalendar
+        username="rasulzeynal"
+        blockSize={15}
+        blockMargin={5}
+        color="#c084f5"
+        fontSize={16}
+      />
+    </Row>
   )
 }
 
