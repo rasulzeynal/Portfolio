b930697224c241c6a6c8358e7602b60fbb75c26f
Rasul <rasul.zeynalov.95@gmail.com>
2022-07-29 16:58:42 +0400

    navbar edited

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -7,15 +7,16 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink} from 'reactstrap';
+  Button} from 'reactstrap';
 import logo from "../assets/img/male.png";
 import {Link} from "react-router-dom";
 import {
     AiOutlineHome,
     AiOutlineUser,
-    AiOutlineFundProjectionScreen
+    AiOutlineFundProjectionScreen,
+    AiFillStar
 } from "react-icons/ai";
-import {CgFileDocument} from "react-icons/cg"
+import {CgFileDocument,CgGitFork} from "react-icons/cg"
 
 const NavBar = () => {
     const [isOpen,setIsOpen] = useState(false);
@@ -36,26 +37,40 @@ const NavBar = () => {
     window.addEventListener("scroll", scrollHandler);
     return (
       <div>
-        <Navbar  fixed='top'  className={navColour ? "sticky" : "navbar"}  expand="md">
+        <Navbar fixed='top'  className={navColour ? "sticky" : "navbar"}  expand="md">
           <NavbarBrand href="/" className='d-flex'><img src={logo} alt="logo" style={{width:"70px"}} /></NavbarBrand>
-          <NavbarToggler onClick={toggle} />
-          <Collapse isOpen={isOpen} className='collapse' navbar style={{color:"white"}}>
-            <Nav className="ml-auto" navbar >
+          <NavbarToggler onClick={toggle} aria-controls='responsive-navbar-nav'>
+          <span></span>
+          <span></span>
+          <span></span>
+          </NavbarToggler>
+          <Collapse isOpen={isOpen} className='collapse' navbar id="responsive-navbar-nav" >
+            <Nav className="ml-auto" navbar>
               <NavItem>
-                <Link to="/" className='nav-link' style={{cursor:"pointer",color:"white"}}>
+                <Link to="/" className='nav-link' onClick={toggle}>
                 <AiOutlineHome className='mr-1 mb-1'/>Home</Link>
               </NavItem>
               <NavItem >
-                <Link to="/about" className='nav-link'  style={{cursor:"pointer",color:"white"}}>
+                <Link to="/about" className='nav-link'  onClick={toggle}>
                   <AiOutlineUser className='mr-1 mb-1'/>About</Link>
               </NavItem>
               <NavItem>
-                <Link to="/project" className='nav-link' style={{cursor:"pointer",color:"white"}}>
+                <Link to="/project" className='nav-link'  onClick={toggle}>
                     <AiOutlineFundProjectionScreen className='mr-1 mb-1'/>Projects</Link>
               </NavItem>
               <NavItem>
-                <Link to="/resume" className='nav-link' style={{cursor:"pointer",color:"white"}}>
+                <Link to="/resume" className='nav-link'  onClick={toggle}>
                 <CgFileDocument className='mr-1 mb-1' />Resume</Link>
+              </NavItem>
+              <NavItem className="fork-btn nav-item">
+              <Button
+                href="https://github.com/rasulzeynal/Portfolio"
+                target="_blank"
+                className="fork-btn-inner"
+              >
+                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
+                <AiFillStar style={{ fontSize: "1.1em" }} />
+              </Button>
               </NavItem>
             </Nav>
           </Collapse>
