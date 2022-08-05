c70caab8a66d328c49bbbfa6743140fea4caa514
Rasul <rasul.zeynalov.95@gmail.com>
2022-08-05 16:56:34 +0400

    navbar completed

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,15 +1,10 @@
-import React from 'react';
-import { useState } from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  Button} from 'reactstrap';
+import React,{ useState } from 'react';
 import logo from "../assets/img/male.png";
 import {Link} from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 import {
     AiOutlineHome,
     AiOutlineUser,
@@ -17,51 +12,86 @@ import {
     AiFillStar
 } from "react-icons/ai";
 import {CgFileDocument,CgGitFork} from "react-icons/cg"
+import { ImBlog } from "react-icons/im";
+
+
 
 const NavBar = () => {
-    const [isOpen,setIsOpen] = useState(false);
-    const [navColour, updateNavbar] = useState(false);
+  const [expand, setExpand] = useState(false);
+  const [navColour, setNavColour] = useState(false);
 
-
-    const scrollHandler = () => {
-        if (window.scrollY >= 20) {
-          updateNavbar(true);
-        } else {
-          updateNavbar(false);
-        }
-      }
-    const toggle = () => {
-        setIsOpen(!isOpen)
+  const scrollHandler = () => {
+    if (window.scrollY >= 20) {
+      setNavColour(true);
+    } else {
+      setNavColour(false);
     }
+  }
 
-    window.addEventListener("scroll", scrollHandler);
-    return (
-      <Navbar fixed='top'  className={navColour ? "sticky" : "navbar"}  expand="md">
-          <NavbarBrand href="/" className='d-flex'><img src={logo} alt="logo" style={{width:"70px"}} /></NavbarBrand>
-          <NavbarToggler onClick={toggle} >
+  window.addEventListener("scroll", scrollHandler);
+
+  return (
+    <Navbar
+      expanded={expand}
+      fixed="top"
+      expand="md"
+      className={navColour ? "sticky" : "navbar"}
+    >
+      <Container>
+        <Navbar.Brand href="/" className="d-flex">
+          <img src={logo} className="img-fluid logo" alt="brand" style={{width:"70px"}} />
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          onClick={() => {
+            setExpand(expand ? false : "expanded");
+          }}
+        >
           <span></span>
           <span></span>
           <span></span>
-          </NavbarToggler>
-          <Collapse isOpen={isOpen} className='collapse' navbar id="responsive-navbar-nav" >
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <Link to="/" className='nav-link'>
-                <AiOutlineHome className='mr-1 mb-1'/>Home</Link>
-              </NavItem>
-              <NavItem >
-                <Link to="/about" className='nav-link' >
-                  <AiOutlineUser className='mr-1 mb-1'/>About</Link>
-              </NavItem>
-              <NavItem>
-                <Link to="/project" className='nav-link' >
-                    <AiOutlineFundProjectionScreen className='mr-1 mb-1'/>Projects</Link>
-              </NavItem>
-              <NavItem>
-                <Link to="/resume" className='nav-link' >
-                <CgFileDocument className='mr-1 mb-1' />Resume</Link>
-              </NavItem>
-              <NavItem className="fork-btn nav-item">
+        </Navbar.Toggle>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto" defaultActiveKey="#home">
+            <Nav.Item>
+              <Nav.Link as={Link} to="/" onClick={() => setExpand(false)}>
+                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+              </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/about"
+                onClick={() => setExpand(false)}
+              >
+                <AiOutlineUser style={{ marginBottom: "2px" }} /> About
+              </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/project"
+                onClick={() => setExpand(false)}
+              >
+                <AiOutlineFundProjectionScreen
+                  style={{ marginBottom: "2px" }}
+                />{" "}
+                Projects
+              </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/resume"
+                onClick={() => setExpand(false)}
+              >
+                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="fork-btn">
               <Button
                 href="https://github.com/rasulzeynal/Portfolio"
                 target="_blank"
@@ -70,10 +100,11 @@ const NavBar = () => {
                 <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
                 <AiFillStar style={{ fontSize: "1.1em" }} />
               </Button>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     );
   }
   export default NavBar;
@@ -156,3 +187,62 @@ const NavBar = () => {
     </Navbar> 
         
         */
+
+
+    /* 
+    const [isOpen,setIsOpen] = useState(false);
+    const [navColour, updateNavbar] = useState(false);
+
+
+    const scrollHandler = () => {
+        if (window.scrollY >= 20) {
+          updateNavbar(true);
+        } else {
+          updateNavbar(false);
+        }
+      }
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+
+    window.addEventListener("scroll", scrollHandler);
+    return (
+      <Navbar fixed='top'  className={navColour ? "sticky" : "navbar"}  expand="md">
+          <NavbarBrand href="/" className='d-flex'><img src={logo} alt="logo" style={{width:"70px"}} /></NavbarBrand>
+          <NavbarToggler onClick={toggle} >
+          <span></span>
+          <span></span>
+          <span></span>
+          </NavbarToggler>
+          <Collapse isOpen={isOpen} className='collapse' navbar id="responsive-navbar-nav" >
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <Link to="/" className='nav-link'>
+                <AiOutlineHome className='mr-1 mb-1'/>Home</Link>
+              </NavItem>
+              <NavItem >
+                <Link to="/about" className='nav-link' >
+                  <AiOutlineUser className='mr-1 mb-1'/>About</Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/project" className='nav-link' >
+                    <AiOutlineFundProjectionScreen className='mr-1 mb-1'/>Projects</Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/resume" className='nav-link' >
+                <CgFileDocument className='mr-1 mb-1' />Resume</Link>
+              </NavItem>
+              <NavItem className="fork-btn nav-item">
+              <Button
+                href="https://github.com/rasulzeynal/Portfolio"
+                target="_blank"
+                className="fork-btn-inner"
+              >
+                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
+                <AiFillStar style={{ fontSize: "1.1em" }} />
+              </Button>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+    */
