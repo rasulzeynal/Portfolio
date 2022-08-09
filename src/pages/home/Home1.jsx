176c73c7e99498334254802719ad551fa3dcd68f
Rasul <rasul.zeynalov.95@gmail.com>
2022-08-09 21:50:11 +0400

    edited

--- src/pages/home/Home1.jsx
+++ src/pages/home/Home1.jsx
@@ -1,5 +1,8 @@
 import { useTypewriter } from "react-simple-typewriter";
-import 'react-simple-typewriter/dist/index'
+import 'react-simple-typewriter/dist/index';
+import { Button } from "react-bootstrap";
+import pdf from "../../assets/cv/Rasul.pdf";
+import { Link } from "react-router-dom";
 
 const Home1 = () => {
     const {text} = useTypewriter({
@@ -18,8 +21,10 @@ const Home1 = () => {
       </span> 
       </h3>
       <div style={{textAlign:"center"}}>
-      <button className="btn btn-cv">Download CV</button>
+      <Button href={pdf} target="_blank" className="btn btn-cv">Download CV</Button>
+      <Link to="/contact">
       <button className="btn btn-contact">Contact</button>
+      </Link>
     </div>
     </div>
   )
