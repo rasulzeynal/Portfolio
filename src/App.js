206e167cc12615abf6d792686b0780dbe433f5f1
Rasul <rasul.zeynalov.95@gmail.com>
2022-12-15 22:28:25 +0400

    Routing err solved

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Navigate to="/"/>} />
+          <Route path="*" element={<Home />} />
       </Routes>
       <Footer/>
     </div>
