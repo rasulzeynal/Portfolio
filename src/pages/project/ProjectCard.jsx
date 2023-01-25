c909c904937ddbc01eaf0d3e9597da995f65b97e
Rasul <rasul.zeynalov.95@gmail.com>
2023-01-25 13:56:27 +0400

    project card bug solved

--- src/pages/project/ProjectCard.jsx
+++ src/pages/project/ProjectCard.jsx
@@ -1,40 +1,48 @@
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button"
+import Button from "react-bootstrap/Button";
 
 const ProjectCard = (props) => {
-    console.log(props.imgPath)
+  console.log(props.imgPath);
   return (
     <Card className="project-card-view col-xs-12 ">
       <div className="project-card-image">
-        <Card.Img variant="top" src={props.imgPath.url} style={{height:"100%"}}  alt="card-img"/>
+        <Card.Img
+          variant="top"
+          src={props.imgPath.url}
+          style={{ height: "100%" }}
+          alt="card-img"
+        />
       </div>
-    
-    <Card.Body>
-        <Card.Title className="project-card-title">{props.imgPath.name}</Card.Title>
-        <Card.Text >
-          {props.imgPath.desc}
-        </Card.Text>
+      <Card.Body>
+        <Card.Title className="project-card-title">
+          {props.imgPath.name}
+        </Card.Title>
+        <Card.Text>{props.imgPath.desc}</Card.Text>
         <div className="project-card-buttons">
-        <Button className="project-card-button" variant="secondary" href={props.imgPath.github} target="_blank" >
-          <BsGithub /> &nbsp;
-          GitHub
-        </Button>
-        {!props.isBlog && props.imgPath.demo && (
           <Button
-          className="project-card-button"
+            className="project-card-button"
             variant="secondary"
-            href={props.imgPath.demo}
+            href={props.imgPath.github}
             target="_blank"
           >
-            <CgWebsite /> &nbsp;
-            Demo
+            <BsGithub /> &nbsp; GitHub
           </Button>
-        )}</div>
+          {props.imgPath.demo && (
+            <Button
+              className="project-card-button"
+              variant="secondary"
+              href={props.imgPath.demo}
+              target="_blank"
+            >
+              <CgWebsite /> &nbsp; Demo
+            </Button>
+          )}
+        </div>
       </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
